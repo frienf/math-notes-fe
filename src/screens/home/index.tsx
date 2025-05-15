79c775fda49060ce3ebb0a9f614cd976032c0f22
Deepak GOND <45816020+frienf@users.noreply.github.com>
2025-05-15 18:48:16 +0530

    Update index.tsx

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -572,7 +572,7 @@ export default function Home() {
                     <Draggable
                         key={index}
                         defaultPosition={latexPosition}
-                        onStop={(e, data) => setLatexPosition({ x: data.x, y: data.y })}
+                        onStop={(_e, data) => setLatexPosition({ x: data.x, y: data.y })}
                     >
                         <div className="absolute p-2 bg-card text-card-foreground rounded-md shadow-md z-20">
                             <div className="latex-content">{latex}</div>
